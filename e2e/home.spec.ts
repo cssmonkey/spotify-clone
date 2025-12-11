12a3b4ac45d2017627b529dd6e3b58a6c68696ea
Sean Jones <web@sean-jones.co.uk>
2025-12-11 14:57:16 +0000

    fix playwright tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage shows heading and button', async ({ page }) => {
+test('homepage shows heading', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(
     page.getByRole('heading', {
-      name: /To get started, edit the page.tsx file./i,
+      name: /welcome to spotify clone/i,
     }),
   ).toBeVisible();
 });
